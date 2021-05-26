ed3891343c2edb8ecba47bc45246174e359b1632
guess_ana <gesanabiti@gmail.com>
2021-05-26 23:03:26 +0200

    clock added

---
+++ CSS+JS-Clock/app.js
@@ -0,0 +1,9 @@
+const secondHand = document.querySelector('.secondHand')
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360);
+}
+
+setInterval(setDate, 1000);
